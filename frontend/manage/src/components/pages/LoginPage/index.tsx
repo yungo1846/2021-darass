8ad45c23bc4ea5ee083cc379297ca309e86978da
zereight <fullstackmachinelearning@gmail.com>
2021-07-15 20:04:05 +0900

    refactor: 로그인 기능 리팩터링
    - reply-module에 있는 cookie/useLogin/user 타입 복사

--- frontend/manage/src/components/pages/LoginPage/index.tsx
+++ frontend/manage/src/components/pages/LoginPage/index.tsx
@@ -1,35 +1,14 @@
-import { useEffect } from "react";
+import useLogin from "../../../hooks/useLogin";
 import { PALETTE } from "../../../styles/palette";
 import ScreenContainer from "../../../styles/ScreenContainer";
 import Login from "../../templates/Login";
 
-const { Kakao } = window;
-
-const loginWithKakao = () => {
-  Kakao.Auth.login({
-    success: function (authObj: { access_token: string }) {
-      const accessToken = JSON.stringify(authObj.access_token);
-
-      alert(JSON.stringify(accessToken));
-    },
-    fail: function (err: Error) {
-      alert(JSON.stringify(err));
-    }
-  });
-};
-
 const LoginPage = () => {
-  const onLoginWithKakao = () => {
-    loginWithKakao();
-  };
-
-  useEffect(() => {
-    Kakao.init(process.env.KAKAO_JAVASCRIPT_API_KEY);
-  });
+  const { login } = useLogin();
 
   return (
     <ScreenContainer bgColor={PALETTE.PRIMARY}>
-      <Login onLoginWithKakao={onLoginWithKakao} />
+      <Login onLoginWithKakao={login} />
     </ScreenContainer>
   );
 };
