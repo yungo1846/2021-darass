3c35158b01209349929bd9e6c7a9a2ee0875770d
jaeseongDev <qkrwotjd1445@naver.com>
2021-07-18 12:40:28 +0900

    feat: 테스트

--- backend/src/main/java/com/darass/darass/DarassApplication.java
+++ backend/src/main/java/com/darass/darass/DarassApplication.java
@@ -1,10 +1,16 @@
 package com.darass.darass;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DarassApplication {
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DarassApplication.class, args);
